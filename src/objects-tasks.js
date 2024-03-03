a4be7b8b1b3ef19bc4f96938aaf1d0801aaf13b3
Oleksii Bolotin <samkiral73@gmail.com>
2024-03-03 22:37:02 +0100

    completed some tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -32,8 +32,16 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(arr) {
+  return arr.reduce((a, b) => {
+    return Object.entries(b).reduce(
+      (acc, entry) => ({
+        ...acc,
+        [entry[0]]: acc[entry[0]] ? entry[1] + acc[entry[0]] : entry[1],
+      }),
+      a
+    );
+  }, {});
 }
 
 /**
@@ -49,8 +57,10 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = shallowCopy(obj);
+  keys.forEach((key) => delete result[key]);
+  return result;
 }
 
 /**
@@ -65,8 +75,11 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(o1, o2) {
+  return (
+    Object.keys(o1).every((k) => o1[k] === o2[k]) &&
+    Object.keys(o2).every((k) => o1[k] === o2[k])
+  );
 }
 
 /**
@@ -80,8 +93,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
 }
 
 /**
@@ -100,8 +113,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(shallowCopy(obj));
 }
 
 /**
